c50c770567adccb0706cb66c2950724f82616100
VergilOP <2397051530@qq.com>
2022-12-12 19:14:48 +0000

    Update ClassTest1-Solution.hs

--- University/2nd/Funtional Programming/Practice/ClassTest1/ClassTest1-Solution.hs
+++ University/2nd/Funtional Programming/Practice/ClassTest1/ClassTest1-Solution.hs
@@ -5,7 +5,7 @@
 -- see https://wiki.haskell.org/Safe_Haskell
 {-# LANGUAGE Safe #-}
 
-module ClassTest1 (openOrSenior, rot13, digpow, sumDigPow, alphabetWar) where
+module ClassTest1-Solution (openOrSenior, rot13, digpow, sumDigPow, alphabetWar, xbonacci, encrypt, decrypt, wave, solve, abbreviate) where
 
 import Types
 import Data.Char
@@ -82,3 +82,65 @@ alphabetWar' a b (x:xs)
     | x == 'd' = alphabetWar' a (b+2) xs
     | x == 'z' = alphabetWar' a (b+1) xs
     | otherwise = alphabetWar' a b xs
+
+{- Question 6 -}
+
+-- https://www.codewars.com/kata/556e0fccc392c527f20000c5
+
+xbonacci :: Num a => [a] -> Int -> [a]
+xbonacci as n = take n (as ++ (go as))
+    where go s = sum s : go (tail s ++ [sum s])
+
+{- Question 7 -}
+
+-- https://www.codewars.com/kata/57814d79a56c88e3e0000786
+
+encrypt :: String -> Int -> String
+encrypt str n
+    | n <= 0 = str
+    | otherwise = encrypt (oddstr ++ evenstr) (n-1)
+    where newstr = zip str [0..]
+          oddstr = map (fst) (filter (\(x,y) -> odd y) newstr)
+          evenstr = map (fst) (filter (\(x,y) -> even y) newstr)
+
+decrypt :: String -> Int -> String
+decrypt str n
+    | n <= 0 = str
+    | otherwise = decrypt (go fststr sndstr) (n-1)
+    where len = (length str) `div` 2
+          fststr = take len str
+          sndstr = drop len str
+          go [] [] = []
+          go [] (x:[]) = [x]
+          go (y:ys) (x:xs) = x:y:(go ys xs)
+
+{- Question 8 -}
+
+-- https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
+
+wave :: String -> [String]
+wave str = go (length str) newstr
+    where newstr = zip str [0..]
+          go 0 _ = []
+          go n nestr = if fst (nestr !! (length nestr - n)) == ' ' then go (n-1) nestr else map (\(a,b) -> if b == (length nestr - n) then toUpper a else a ) newstr : go (n-1) nestr
+
+{- Question 9 -}
+
+-- https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0
+
+solve :: [String] -> [Int]
+solve xs = map (\ys -> go (map toLower ys) ['a'..'z']) xs
+    where go [] _ = 0
+          go (a:as) (b:bs) = if a == b then 1 + go as bs else go as bs
+
+{- Question 10 -}
+
+-- https://www.codewars.com/kata/5375f921003bf62192000746
+
+abbreviate :: String -> String
+abbreviate [] = [] 
+abbreviate str = if b == [] then trans a else trans a ++ head b : abbreviate (tail b)
+    where (a,b) = break (\x-> not (isLetter x)) str
+          trans n
+              | length n < 4 = n
+              | otherwise = head n: show (length n - 2) ++ [last n]
